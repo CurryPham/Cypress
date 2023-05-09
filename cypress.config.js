70883f8df6aa0f4717c5c5c38ac30dc30fdb976a
Curry Pham <khoapd2000@gmail.com>
2023-05-09 14:22:35 +0700

    [within] and [find] method

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '167q5u',
+  projectId: 'jmsg8d',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 
-    projectId: "cypress",
     specPattern: "./cypress/tests/**.*",
     // baseUrl: "https://the-internet.herokuapp.com/",
     baseUrl: "https://www.amazon.com/",
