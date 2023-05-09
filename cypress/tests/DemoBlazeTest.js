0a26da2d002d39712fc00580307f7df8980448bd
Curry Pham <khoapd2000@gmail.com>
2023-05-09 22:15:02 +0700

    Intercept the request and verify UI data against backend data

--- cypress/tests/DemoBlazeTest.js
+++ cypress/tests/DemoBlazeTest.js
@@ -1,3 +1,4 @@
+import { entries } from "lodash";
 import DemoBlazePage from "../models/pages/DemoBlazePage";
 import products from "./product.json"
 
@@ -9,6 +10,26 @@ describe('SR HomePage', () => {
             cy.wrap('').then(() => {
                 //cy.log(JSON.stringify(allCarData))
                 expect(allCardData).to.be.deep.eq(products);
+            })
+        })
+    });
+
+    it.only('should be able to get hero card title', () => {
+        cy.visit("https://www.demoblaze.com/");
+        cy.intercept('/entries').as('entries')
+        cy.wait('@entries')
+        cy.get('@entries').then(entries => {
+            let apiProductData = entries.response.body.Items
+            apiProductData = apiProductData.map(item => {
+                return {
+                    itemName: item.title.replace('\n', ''),
+                    itemPrice: `$${item.price}`
+                }
+            })
+            new DemoBlazePage().getAllCardData().then(allCardData => {
+                cy.wrap('').then(() => {
+                    expect(allCardData).to.be.deep.eq(apiProductData);
+                })
             })
         })
     });
