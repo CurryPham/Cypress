c5fab8e4010cc25d710fc16c911ed1408b1abcc7
Curry Pham <curry.pham@alpaca.vn>
2023-05-12 15:00:56 +0700

    Test report and custom steps to handle test report

--- cypress/tests/order/OrderItem.js
+++ cypress/tests/order/OrderItem.js
@@ -11,60 +11,60 @@ describe('Product Component Test', () => {
     headerComp = new HeaderComponent();
     loginComp = new LoginComponent();
   })
-  
+
   const login = (username, password) => {
-    headerComp.getLoginLink().click({force: true});
+    headerComp.getLoginLink().click({ force: true });
     loginComp.getLoginModal().should('be.visible')
-    loginComp.getUsername().type(`${username}`, {force: true, waitForAnimations: true});
-    loginComp.getPassword().type(`${password}`, {force: true, waitForAnimations: true});
-    loginComp.getLoginBtn().click({force: true});
+    loginComp.getUsername().type(`${username}`, { force: true, waitForAnimations: true });
+    loginComp.getPassword().type(`${password}`, { force: true, waitForAnimations: true });
+    loginComp.getLoginBtn().click({ force: true });
   }
 
   it('oder item as guest', () => {
     purchaseItem();
   });
-  
-    it('oder item as user', () => {
-      const LOGIN_CRED = {
-        username: "tun", 
-        password: "admin"
-      }
-      const {username, password} = LOGIN_CRED;
-      login(username, password);
-      purchaseItem();
-    });
+
+  it('oder item as user', () => {
+    const LOGIN_CRED = {
+      username: "tun",
+      password: "admin"
+    }
+    const { username, password } = LOGIN_CRED;
+    login(username, password); 
+    purchaseItem();
   });
+});
 
 
-  const purchaseItem = () => {
-    HomePageAPI.getHomePageProduct().then(apiData => {
-      const ranDomProduct = apiData[Math.floor(Math.random() * apiData.length)];
-      const ranDomProductTitle = ranDomProduct.title.trim().replace("\n", "");
-      cy.contains(ranDomProductTitle).click();
+const purchaseItem = () => {
+  HomePageAPI.getHomePageProduct().then(apiData => {
+    const ranDomProduct = apiData[Math.floor(Math.random() * apiData.length)];
+    const ranDomProductTitle = ranDomProduct.title.trim().replace("\n", "");
+    cy.contains(ranDomProductTitle).click();
 
-      // CLick on add to cart button
-      cy.contains('Add to cart').click();
+    // CLick on add to cart button
+    cy.contains('Add to cart').click();
 
-      // Go to cart
-      cy.get('#cartur').click();
+    // Go to cart
+    cy.get('#cartur').click();
 
-      // Place order
-      cy.contains('Place Order').click();
+    // Place order
+    cy.contains('Place Order').click();
 
-      // Place order drtails
-      cy.get('#name').type('Tun');
-      cy.get('#country').type('Vietnam');
-      cy.get('#city').type('HCM');
-      cy.get('#card').type('123456789');
-      cy.get('#month').type('02');
-      cy.get('#year').type('09');
-      cy.contains('Purchase').click();
+    // Place order drtails
+    cy.get('#name').type('Tun');
+    cy.get('#country').type('Vietnam');
+    cy.get('#city').type('HCM');
+    cy.get('#card').type('123456789');
+    cy.get('#month').type('02');
+    cy.get('#year').type('09');
+    cy.contains('Purchase').click();
 
-      // Verify 
-      cy.get('.sweet-alert h2').should('have.text', 'Thank you for your purchase!')
-      cy.get('.sweet-alert .lead').then($confirmOrderDetails => {
-        cy.wrap($confirmOrderDetails).should('contain.text', ranDomProduct.price)
-        cy.wrap($confirmOrderDetails).should('contain.text', 'Card Number: 123456789')
-      })
+    // Verify 
+    cy.get('.sweet-alert h2').should('have.text', 'Thank you for your purchase!')
+    cy.get('.sweet-alert .lead').then($confirmOrderDetails => {
+      cy.wrap($confirmOrderDetails).should('contain.text', ranDomProduct.price)
+      cy.wrap($confirmOrderDetails).should('contain.text', 'Card Number: 123456789')
     })
-  }
+  })
+}
